7408e3835df175c678162f102d9b37e495fe3b20
Brazilianian <vetajika@gmail.com>
2025-02-12 15:02:20 +0200

    feat: added tests for ice and fire

--- src/api/anapioficeandfire.test.js
+++ src/api/anapioficeandfire.test.js
@@ -1,29 +1,39 @@
 /* eslint-env jest */
 
-const apiIceAndFire = require('./anapioficeandfire')
+const {getBooks, getListOfRestEndPoint} = require("./anapioficeandfire");
 
-jest.mock('./anapioficeandfire', () => {
-    const originalModule = jest.requireActual('./anapioficeandfire');
-    const resp = require('../__mocksData__/api.json')
-    return {
-        __esModule: true,
-        ...originalModule,
-        getListOfRestEndPoint: function () {
-            return new Promise((resolve, reject) => {
-                resolve({entity: resp})
-            })
-        },
-    };
-});
+jest.mock('./anapioficeandfire')
 
-describe('#getBooks() using Promises', () => {
-    it('should load books data', () => {
-        apiIceAndFire.getListOfRestEndPoint()
-            .then(data => {
-                expect(data.entity.books).toBeDefined()
-                expect(data.entity.books).toEqual('https://www.anapioficeandfire.com/api/books')
-                expect(data.entity.houses).toBeDefined()
-                expect(data.entity.houses).toEqual('https://www.anapioficeandfire.com/api/houses')
-            })
-    })
+describe('Test suits for API Ice And Fire', () => {
+    test('getBooks() method must return correct data', async () => {
+        getBooks.mockResolvedValueOnce({
+            entity: {
+                "A Game of Thrones": "https://www.anapioficeandfire.com/api/books/1",
+                "A Clash of Kings": "https://www.anapioficeandfire.com/api/books/2",
+            },
+        });
+
+        const books = await getBooks();
+        expect(books.entity).toHaveProperty('A Game of Thrones');
+        expect(books.entity['A Game of Thrones']).toBe(
+            'https://www.anapioficeandfire.com/api/books/1'
+        );
+    });
+
+    test('getListOfRestEndPoint() method mst return correct data', async () => {
+        getListOfRestEndPoint.mockResolvedValueOnce({
+            entity: {
+                books: 'https://www.anapioficeandfire.com/api/books',
+                characters: 'https://www.anapioficeandfire.com/api/characters',
+                houses: 'https://www.anapioficeandfire.com/api/houses',
+            },
+        });
+
+        const endPoints = await getListOfRestEndPoint();
+        expect(endPoints.entity).toHaveProperty('houses');
+        expect(endPoints.entity.houses).toBe(
+            'https://www.anapioficeandfire.com/api/houses'
+        );
+    });
 })
+
